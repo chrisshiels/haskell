3cbe1628b468f23e3383ca7e5066ac7e2d99a23f
Chris Shiels <chris@systems-engineers.co.uk>
2019-08-13 22:25:54 +0100

    Add primes.hs.

---
+++ primes.hs
@@ -0,0 +1,39 @@
+-- 'primes.hs'.
+
+
+import Data.List
+import System.Environment
+import System.Exit
+import Text.Read
+
+
+notmultipleof :: Int -> Int -> Bool
+notmultipleof x y = y `mod` x /= 0
+
+
+sieve :: [ Int ] -> [ Int ]
+sieve [] = []
+sieve (x:xs) = [ x ] ++ (sieve . filter (notmultipleof x) $ xs)
+
+
+primes :: Int -> IO ExitCode
+primes n = (putStrLn . intercalate "\n" . map show . sieve $ [2..n]) >>
+           return ExitSuccess
+
+
+usage :: ExitCode -> IO ExitCode
+usage e = putStrLn "Usage:  primes n" >>
+          return e
+
+
+app :: [ String ] -> IO ExitCode
+app [ "--help" ] = usage ExitSuccess
+app []           = usage ExitSuccess
+app [ n ]        = case readMaybe n :: Maybe Int of
+                       Just n  -> primes n
+                       Nothing -> usage (ExitFailure 1)
+app _            = usage (ExitFailure 1)
+
+
+main :: IO ExitCode
+main = getArgs >>= app >>= exitWith
