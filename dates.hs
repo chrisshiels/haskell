43eebf64b8ddf25d1b3fe8a9fa0185c0dce6151c
Chris Shiels <chris@systems-engineers.co.uk>
2019-08-17 11:38:48 +0100

    Add dates.hs.

---
+++ dates.hs
@@ -0,0 +1,26 @@
+-- 'dates.hs'.
+
+
+import Data.Time
+import System.Exit
+
+
+date1 :: IO String
+date1 = do t <- getZonedTime
+           let s = formatTime defaultTimeLocale "%c" t
+           return s
+
+
+date2 :: IO String
+date2 = fmap (formatTime defaultTimeLocale "%c") getZonedTime
+
+
+date3 :: IO String
+date3 = formatTime defaultTimeLocale "%c" <$> getZonedTime
+
+
+main :: IO ExitCode
+main = date1 >>= putStrLn >>
+       date2 >>= putStrLn >>
+       date3 >>= putStrLn >>
+       exitWith ExitSuccess
