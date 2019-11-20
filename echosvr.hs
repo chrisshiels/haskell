abe3e52d43188950ca249b1b4d9d462aec288033
Chris Shiels <chris@systems-engineers.co.uk>
2019-11-20 23:08:21 +0000

    Add echosvr.hs.

---
+++ echosvr.hs
@@ -0,0 +1,56 @@
+-- 'echosvr.hs'.
+
+
+import Control.Exception
+import Data.ByteString
+import Network.Socket
+import Network.Socket.ByteString
+import System.Environment
+import System.Exit
+
+
+getsocket :: PortNumber -> IO Socket
+getsocket port =
+  do s <- socket AF_INET Stream defaultProtocol
+     setSocketOption s ReuseAddr 1
+     bind s (SockAddrInet port 0)
+     listen s 1024
+     return s
+
+
+loop :: (Socket -> IO ()) -> Socket -> IO ()
+loop action socket =
+  do (conn, _) <- accept socket
+     action conn
+     loop action socket
+
+
+action :: Socket -> IO ()
+action socket =
+  do msg <- Network.Socket.ByteString.recv socket 1024
+     if Data.ByteString.null msg
+     then return ()
+     else do Network.Socket.ByteString.sendAll socket msg
+             action socket
+
+
+echosvr :: PortNumber -> IO ExitCode
+echosvr port =
+  bracket (getsocket port) close (loop action) >>
+  return ExitSuccess
+
+
+usage :: ExitCode -> IO ExitCode
+usage e = Prelude.putStrLn "Usage:  echosvr [ port ]" >>
+          return e
+
+
+app :: [ String ] -> IO ExitCode
+app [ "--help" ] = usage ExitSuccess
+app []           = echosvr 7777
+app [ s ]        = echosvr (read s :: PortNumber)
+app _            = usage (ExitFailure 1)
+
+
+main :: IO ExitCode
+main = getArgs >>= app >>= exitWith
